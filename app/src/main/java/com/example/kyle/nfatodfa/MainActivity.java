e07c800f39b9a354652daab5d524088a008d5ca9
Kyle <kwill036@plattsburgh.edu>
2015-10-20 04:01:23 -0400

    MainActivity layout is complete, ready to transition into the next layout

--- app/src/main/java/com/example/kyle/nfatodfa/MainActivity.java
+++ app/src/main/java/com/example/kyle/nfatodfa/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.kyle.nfatodfa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +22,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stateNames = ((EditText) findViewById(R.id.stateNamesEdit)).getText().toString();
+                String symbols = ((EditText) findViewById(R.id.symbolsEdit)).getText().toString();
+                String startingState = ((EditText) findViewById(R.id.startStateEdit)).getText().toString();
+                String finalStates = ((EditText) findViewById(R.id.finalStatesEdit)).getText().toString();
+                if(stateNames.equals("") | symbols.equals("") | startingState.equals("") | finalStates.equals("")){
+                    Toast.makeText(getApplicationContext(), "Please complete all fields",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    String[] stateNamesParsed = stateNames.split(" ");
+                    String[] symbolsParsed = symbols.split(" ");
+                    String[] finalStatesParsed = finalStates.split(" ");
+                    for(String state : stateNamesParsed){
+                        for(String symbol : symbolsParsed){
+                            String hi = "\uD835\uDEFF(" + state + ", " + symbol + ") =>";
+                            Toast.makeText(getApplicationContext(), hi, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
     }
 
     @Override
